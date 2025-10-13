e0cc014a93662c0493303cac6c399a2f99a2cf0e
Your Name <you@example.com>
2025-10-13 01:22:06 -0400

    Added routes

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,3 +1,56 @@
+const express = require('express');
+const router = express.Router();
+const connectToDatabase = require('../models/db');
+const logger = require('../logger');
+
+// Get all gifts
+router.get('/', async (req, res, next) => {
+    logger.info('/ called');
+    try {
+        const db = await connectToDatabase();
+
+        const collection = db.collection("gifts");
+        const gifts = await collection.find({}).toArray();
+        res.json(gifts);
+    } catch (e) {
+        logger.console.error('oops something went wrong', e)
+        next(e);
+    }
+});
+
+// Get a single gift by ID
+router.get('/:id', async (req, res, next) => {
+    try {
+        const db = await connectToDatabase();
+        const collection = db.collection("gifts");
+        const id = req.params.id;
+        const gift = await collection.findOne({ id: id });
+
+        if (!gift) {
+            return res.status(404).send("Gift not found");
+        }
+
+        res.json(gift);
+    } catch (e) {
+        next(e);
+    }
+});
+
+
+// Add a new gift
+router.post('/', async (req, res, next) => {
+    try {
+        const db = await connectToDatabase();
+        const collection = db.collection("gifts");
+        const gift = await collection.insertOne(req.body);
+
+        res.status(201).json(gift.ops[0]);
+    } catch (e) {
+        next(e);
+    }
+});
+
+module.exports = router;
 router.get('/', async (req, res) => {
     try {
         // Task 1: Connect to MongoDB and store connection to db constant
@@ -36,7 +89,7 @@ router.get('/:id', async (req, res) => {
         // Task 3: Find a specific gift by ID using the collection.fineOne method and store in constant called gift
         // {{insert code here}}
         const gift = await collection.findOne({ id: id });
-        
+
         if (!gift) {
             return res.status(404).send('Gift not found');
         }
